65fce10ff865f44991ae692a55267d954d5442be
sgenini <sgenini@gmail.com>
2018-12-13 12:57:59 -0800

    Add SQL query to create department table

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,9 @@ CREATE TABLE products (
     stock_quantity INT NULL,
     PRIMARY KEY (item_id)
 )
+
+CREATE TABLE departments (
+    department_id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(100) NULL,
+    over_head_costs DECIMAL(10,2) NULL
+)
